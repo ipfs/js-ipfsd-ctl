6977aa0c0c5074343a06f3924353ebbddfa87408
JGAntunes <j.goncalo.antunes@gmail.com>
2016-07-12 03:09:42 +0100

    binary wrapper finally working

--- src/node.js
+++ src/node.js
@@ -13,55 +13,40 @@ const join = path.join
 const BinWrapper = require('bin-wrapper')
 
 // TODO should go to config
-const DIST_BASE = 'https://dist.ipfs.io/go-ipfs/v0.4.2/go-ipfs_v0.4.2_darwin-386.tar.gz'
-const VERSION = 'v0.4.2'
+const DIST_BASE = 'https://dist.ipfs.io/go-ipfs'
+const VERSION = 'v0.3.11'
 const PROJECT_BASE = DIST_BASE + '/' + VERSION + '/go-ipfs_' + VERSION + '_'
+const IPFS_DEFAULT_EXEC_PATH = path.join(__dirname, '..', 'vendor')
+const IPFS_EXEC_PATH = process.env.IPFS_EXEC || IPFS_DEFAULT_EXEC_PATH
+
 const bin = new BinWrapper()
-    .src(PROJECT_BASE + '_darwin-386.tar.gz', 'darwin', '386')
-    .src(PROJECT_BASE + '_darwin-amd64.tar.gz', 'darwin', 'amd64')
-    .src(PROJECT_BASE + '_freebsd-amd64.tar.gz', 'freebsd', 'amd64')
-    .src(PROJECT_BASE + '_linux-386.tar.gz', 'linux', '386')
-    .src(PROJECT_BASE + '_linux-amd64.tar.gz', 'linux', 'amd64')
-    .src(PROJECT_BASE + '_linux-arm.tar.gz', 'linux', 'arm')
-    .src(PROJECT_BASE + '_windows-386.tar.gz', 'win32', '386')
-    .src(PROJECT_BASE + '_windows-amd64.tar.gz', 'win32', 'amd64')
+    .src(PROJECT_BASE + 'darwin-386.tar.gz', 'darwin', 'ia32')
+    .src(PROJECT_BASE + 'darwin-amd64.tar.gz', 'darwin', 'x64')
+    .src(PROJECT_BASE + 'freebsd-amd64.tar.gz', 'freebsd', 'x64')
+    .src(PROJECT_BASE + 'linux-386.tar.gz', 'linux', 'ia32')
+    .src(PROJECT_BASE + 'linux-amd64.tar.gz', 'linux', 'x64')
+    .src(PROJECT_BASE + 'linux-arm.tar.gz', 'linux', 'arm')
+    .src(PROJECT_BASE + 'windows-386.tar.gz', 'win32', 'ia32')
+    .src(PROJECT_BASE + 'windows-amd64.tar.gz', 'win32', 'x64')
     // TODO handle dest folder
-    .dest(path.join(__dirname, '..', 'vendor'))
+    .dest(IPFS_EXEC_PATH)
     .use(process.platform === 'win32' ? 'ipfs.exe' : 'ipfs')
     // .version('>=1.71');
 
-bin.run(['--version'], err => {
-    console.log('gifsicle is working');
-});
+// const rootPath = process.env.testpath ? process.env.testpath : __dirname
 
-const rootPath = process.env.testpath ? process.env.testpath : __dirname
-
-const ipfsDefaultPath = findIpfsExecutable()
+// console.log(PROJECT_BASE + 'linux-amd64.tar.gz')
 
 const GRACE_PERIOD = 7500 // amount of ms to wait before sigkill
 
-function findIpfsExecutable () {
-  let npm3Path = path.join(rootPath, '../../', '/go-ipfs-dep/go-ipfs/ipfs')
-
-  let npm2Path = path.join(rootPath, '../', 'node_modules/go-ipfs-dep/go-ipfs/ipfs')
-
-  try {
-    fs.statSync(npm3Path)
-    return npm3Path
-  } catch (e) {
-    return npm2Path
-  }
-}
-
 function configureNode (node, conf, done) {
   const keys = Object.keys(conf)
+  // console.log('CONFIG NODE', conf)
   series(keys.map((key) => (cb) => {
     const value = conf[key]
     const env = {env: node.env}
 
-    run(node.exec, ['config', key, '--json', JSON.stringify(value)], env)
-      .on('error', cb)
-      .on('end', cb)
+    node._run(['config', key, '--json', JSON.stringify(value)], env, cb)
   }), done)
 }
 
@@ -76,145 +61,177 @@ function parseConfig (path, done) {
   }
 }
 
-module.exports = class Node {
-  constructor (path, opts, disposable) {
-    this.path = path
-    this.opts = opts || {}
-    this.exec = process.env.IPFS_EXEC || ipfsDefaultPath
-    this.subprocess = null
-    this.initialized = fs.existsSync(path)
-    this.clean = true
-    this.env = Object.assign({}, process.env, {IPFS_PATH: path})
+function Node (path, opts, disposable) {
+  this.path = path
+  this.opts = opts || {}
+  this.exec = bin.path()
+  this.subprocess = null
+  this.initialized = fs.existsSync(path)
+  this.clean = true
+  // Has the binary been checked?
+  this.checked = false
 
-    if (this.opts.env) Object.assign(this.env, this.opts.env)
-  }
+  this.env = Object.assign({}, process.env, {IPFS_PATH: path})
+  if (this.opts.env) Object.assign(this.env, this.opts.env)
+}
 
-  _run (args, envArg, done) {
-    let result = ''
-    run(this.exec, args, envArg)
-      .on('error', done)
-      .on('data', (data) => {
+Node.prototype._run = function _run (args, env, listeners, done) {
+  let result = ''
+  let callback
+  // console.log('RUNNING - ', args, listeners)
+  // Handy method if we just want the result and err returned in a callback
+  if (typeof listeners === 'function') {
+    callback = listeners
+    listeners = {
+      error: callback,
+      data: (data) => {
         result += data
-      })
-      .on('end', () => done(null, result.trim()))
-  }
-
-  init (initOpts, done) {
-    if (!done) {
-      done = initOpts
-      initOpts = {}
-    }
-    let buf = ''
-
-    const keySize = initOpts.keysize || 2048
-
-    if (initOpts.directory && initOpts.directory !== this.path) {
-      this.path = initOpts.directory
-      this.env.IPFS_PATH = this.path
-    }
-
-    run(this.exec, ['init', '-b', keySize], {env: this.env})
-      .on('error', done)
-      .on('data', (data) => {
-        buf += data
-      })
-      .on('end', () => {
-        configureNode(this, this.opts, (err) => {
-          if (err) return done(err)
-          this.clean = false
-          this.initialized = true
-          done(null, this)
-        })
-      })
-
-    if (this.disposable) {
-      shutdown.addHandler('disposable', 1, this.shutdown.bind(this))
+      },
+      end: () => callback(null, result.trim())
     }
   }
-
-  // cleanup tmp files
-  shutdown (done) {
-    if (!this.clean && this.disposable) {
-      rimraf(this.path, (err) => {
-        if (err) throw err
-        done()
+  // console.log('SETTING LISTENERS', listeners)
+  // console.log('EXEC', this.exec)
+  series([
+    // Check the binary and download it if needed be
+    (cb) => {
+      if (this.checked) return cb()
+      bin.run(['version'], (err) => {
+        if (!err) this.checked = true
+        return cb(err)
       })
     }
+  ], (err, results) => {
+
+    if (err) {
+      // If no done callback return error to the error listener
+      if (!done) return listeners.error(err)
+      return done(err)
+    }
+    // console.log('GOING TO RUN >>>', this.exec, args)
+    const process = run(this.exec, args, env)
+    const listenerKeys = Object.keys(listeners)
+    listenerKeys.forEach((key) => process.on(key, listeners[key]))
+    // If done callback return process
+    if (done) return done(null, process)
+  })
+}
+
+Node.prototype.init = function init (initOpts, done) {
+  if (!done) {
+    done = initOpts
+    initOpts = {}
   }
 
-  startDaemon (done) {
-    parseConfig(this.path, (err, conf) => {
+  const keySize = initOpts.keysize || 2048
+
+  if (initOpts.directory && initOpts.directory !== this.path) {
+    this.path = initOpts.directory
+    this.env.IPFS_PATH = this.path
+  }
+
+  this._run(['init', '-b', keySize], {env: this.env}, (err, result) => {
+    if (err) return done(err)
+    configureNode(this, this.opts, (err) => {
       if (err) return done(err)
-
-      this.subprocess = run(this.exec, ['daemon'], {env: this.env})
-        .on('error', (err) => {
-          if (String(err).match('daemon is running')) {
-            // we're good
-            done(null, ipfs(conf.Addresses.API))
-          } else if (String(err).match('non-zero exit code')) {
-            // ignore when kill -9'd
-          } else {
-            done(err)
-          }
-        })
-        .on('data', (data) => {
-          const match = String(data).trim().match(/API server listening on (.*)/)
-          if (match) {
-            this.apiAddr = match[1]
-            const addr = multiaddr(this.apiAddr).nodeAddress()
-            const api = ipfs(this.apiAddr)
-            api.apiHost = addr.address
-            api.apiPort = addr.port
-            done(null, api)
-          }
-        })
+      this.clean = false
+      this.initialized = true
+      done(null, this)
     })
-  }
+  })
 
-  stopDaemon (done) {
-    if (!done) done = () => {}
-    if (!this.subprocess) return done(null)
-
-    this.subprocess.kill('SIGTERM')
-
-    const timeout = setTimeout(() => {
-      this.subprocess.kill('SIGKILL')
-      done(null)
-    }, GRACE_PERIOD)
-
-    this.subprocess.on('close', () => {
-      clearTimeout(timeout)
-      done(null)
-    })
-
-    this.subprocess = null
-  }
-
-  daemonPid () {
-    return this.subprocess && this.subprocess.pid
-  }
-
-  getConfig (key, done) {
-    if (typeof key === 'function') {
-      done = key
-      key = ''
-    }
-
-    this._run(['config', key], {env: this.env}, done)
-  }
-
-  setConfig (key, value, done) {
-    run(this.exec, ['config', key, value, '--json'], {env: this.env})
-      .on('error', done)
-      .on('data', (data) => {})
-      .on('end', () => done())
-  }
-
-  replaceConf (file, done) {
-    this._run(['config', 'replace', file], {env: this.env}, done)
-  }
-
-  version (done) {
-    this._run(['version'], {}, done)
+  if (this.disposable) {
+    shutdown.addHandler('disposable', 1, this.shutdown.bind(this))
   }
 }
+
+// cleanup tmp files
+Node.prototype.shutdown = function shutdown (done) {
+  if (!this.clean && this.disposable) {
+    rimraf(this.path, (err) => {
+      if (err) throw err
+      done()
+    })
+  }
+}
+
+Node.prototype.startDaemon = function startDaemon (done) {
+  parseConfig(this.path, (err, conf) => {
+    if (err) return done(err)
+
+    this._run(['daemon'], {env: this.env}, {
+      error: (err) => {
+        // console.log('<<<<< START DAEMON - ' + data)
+        if (String(err).match('daemon is running')) {
+          // we're good
+          done(null, ipfs(conf.Addresses.API))
+        } else if (String(err).match('non-zero exit code')) {
+          // ignore when kill -9'd
+        } else {
+          done(err)
+        }
+      },
+      data: (data) => {
+        // console.log('<<<<< START DAEMON - ' + data)
+        const match = String(data).trim().match(/API server listening on (.*)/)
+        if (match) {
+          this.apiAddr = match[1]
+          const addr = multiaddr(this.apiAddr).nodeAddress()
+          const api = ipfs(this.apiAddr)
+          api.apiHost = addr.address
+          api.apiPort = addr.port
+          done(null, api)
+        }
+      }
+    }, (err, process) => {
+      if (err) return done(err)
+      this.subprocess = process
+    })
+  })
+}
+
+Node.prototype.stopDaemon = function stopDaemon (done) {
+  if (!done) done = () => {}
+  if (!this.subprocess) return done(null)
+
+  this.subprocess.kill('SIGTERM')
+
+  const timeout = setTimeout(() => {
+    this.subprocess.kill('SIGKILL')
+    done(null)
+  }, GRACE_PERIOD)
+
+  this.subprocess.on('close', () => {
+    clearTimeout(timeout)
+    done(null)
+  })
+
+  this.subprocess = null
+}
+
+Node.prototype.daemonPid = function daemonPid () {
+  return this.subprocess && this.subprocess.pid
+}
+
+Node.prototype.getConfig = function getConfig (key, done) {
+  if (typeof key === 'function') {
+    done = key
+    key = ''
+  }
+
+  this._run(['config', key], {env: this.env}, done)
+}
+
+Node.prototype.setConfig = function setConfig (key, value, done) {
+  this._run(['config', key, value, '--json'], {env: this.env}, done)
+}
+
+Node.prototype.replaceConf = function replaceConf (file, done) {
+  this._run(['config', 'replace', file], {env: this.env}, done)
+}
+
+Node.prototype.version = function version (done) {
+  this._run(['version'], {}, done)
+}
+
+module.exports = Node
