50e2b981365ab74114db88372b207b68e5facc45
Dmitriy Ryajov <dryajov@gmail.com>
2017-12-07 16:43:54 -0600

    wip: examples

--- examples/local-disposable.js
+++ examples/local-disposable.js
@@ -1,24 +1,32 @@
 /* eslint no-console: 0 */
 'use strict'
 
+// Start a disposable node, and get access to the api
+// print the node id
+
+// IPFS_PATH will point to /tmp/ipfs_***** and will be
+// cleaned up when the process exits.
+
 const factory = require('../')
 const localController = factory.localController
 
-localController.spawn(function (err, ipfsd) {
+// start a go daemon
+localController.spawn((err, ipfsd) => {
   const ipfs = ipfsd.ctl
   const node = ipfsd.ctrl
   ipfs.id(function (err, id) {
-    console.log('alice')
+    console.log('go-ipfs')
     console.log(id)
     node.stopDaemon()
   })
 })
 
-localController.spawn(function (err, ipfsd) {
+// start a js daemon
+localController.spawn({ isJs: true }, (err, ipfsd) => {
   const ipfs = ipfsd.ctl
   const node = ipfsd.ctrl
   ipfs.id(function (err, id) {
-    console.log('bob')
+    console.log('js-ipfs')
     console.log(id)
     node.stopDaemon()
   })
