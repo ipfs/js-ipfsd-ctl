42597c17d3fa9d0ade378ab019d7e18216904691
David Dias <daviddias.p@gmail.com>
2018-05-14 21:06:16 +0100

    feat: let js-ipfs create a repo for itself, no need to import ipfs-repo

--- src/ipfsd-in-proc.js
+++ src/ipfsd-in-proc.js
@@ -2,7 +2,6 @@
 
 const multiaddr = require('multiaddr')
 const defaultsDeep = require('lodash.defaultsdeep')
-const createRepo = require('./utils/repo/create-nodejs')
 const defaults = require('lodash.defaults')
 const waterfall = require('async/waterfall')
 const debug = require('debug')
@@ -31,7 +30,6 @@ class Node extends EventEmitter {
 
     this.opts.args = this.opts.args || []
     this.path = this.opts.repoPath
-    this.repo = createRepo(this.path)
     this.disposable = this.opts.disposable
     this.clean = true
     this._apiAddr = null
@@ -64,7 +62,7 @@ class Node extends EventEmitter {
     })
 
     this.exec = new IPFS({
-      repo: this.repo,
+      repo: this.path,
       init: false,
       start: false,
       pass: this.opts.pass,
