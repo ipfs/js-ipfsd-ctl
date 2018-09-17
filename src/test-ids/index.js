02430d6f7d44e7973f27918c3cbbdef958c94e17
Maciej Krüger <mkg20001@gmail.com>
2018-09-17 21:00:48 +0200

    fix: apply requested changes

--- src/test-ids/index.js
+++ src/test-ids/index.js
@@ -1,5 +1,11 @@
 'use strict'
 
+const protobuf = require('protons')
+const pbm = protobuf(require('libp2p-crypto/src/keys/keys.proto'))
+
+const ids = shuffle(require('./ids.json').split('"')) // file can be generated using mkg20001/test-peer-ids.tk patched with ipfs://QmVZRWjZoqve9UKgng2gymN8a3FQUENgsQaicQ2fWs7kGx
+const base = require('base-x')(' !#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~') // "compression" aka "whatever part of ascii can be put into a JSON string without needing to get escaped"-base
+
 /**
  * Shuffles array in place.
  * Credit: https://stackoverflow.com/a/6274381/3990041
@@ -17,19 +23,13 @@ function shuffle (a) {
   return a
 }
 
-const Ids = shuffle(require('./ids.json').split('"')) // file can be generated using mkg20001/test-peer-ids.tk patched with ipfs://QmVZRWjZoqve9UKgng2gymN8a3FQUENgsQaicQ2fWs7kGx
-const base = require('base-x')(' !#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~') // "compression" aka "whatever part of ascii can be put into a JSON string without needing to get escaped"-base
-
-const protobuf = require('protons')
-const pbm = protobuf(require('libp2p-crypto/src/keys/keys.proto'))
-
 module.exports = () => {
-  if (!Ids.length) { // TODO: should this maybe re-use ids?
-    throw new Error('Ran out of pregenerated ids! What do you even need this many for?')
+  if (!ids.length) { // TODO: should this maybe re-use ids?
+    throw new Error('Ran out of pregenerated IDs! What do you even need this many for?')
   }
 
   return pbm.PublicKey.encode({ // this is needed because the keys aren't wrapped in the usual privateKey pbuf container to save space
     Type: pbm.KeyType.RSA,
-    Data: base.decode(Ids.pop())
+    Data: base.decode(ids.pop())
   }).toString('base64')
 }
