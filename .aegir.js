91caf7cb2ca6286af5a70b02df3e0c87b1182c6a
Hugo Dias <hugomrdias@gmail.com>
2018-10-16 18:52:48 +0100

    chore: add ci file
    chore: add git
    
    chore: normal node img
    
    chore: remove apk cmd
    
    chore: browser tests
    
    chore: fix type
    
    chore: add lock and fix bugs
    
    chore: add lock and new browser img
    
    chore: new browser img
    
    chore: add lock
    
    chore: yarn
    
    chore: fix chrome
    
    chore: add firefox
    
    chore: remove npm lock
    
    chore: lock
    
    chore: lock
    
    add commitlint
    
    fix commit lint
    
    cc

--- .aegir.js
+++ .aegir.js
@@ -5,6 +5,19 @@ const createServer = require('./src').createServer
 const server = createServer() // using defaults
 module.exports = {
   karma: {
+    customLaunchers: {
+      ChromeDocker: {
+        base: 'ChromeHeadless',
+        // We must disable the Chrome sandbox when running Chrome inside Docker (Chrome's sandbox needs
+        // more permissions than Docker allows by default)
+        flags: ['--no-sandbox']
+      }
+    },
+    client: {
+      mocha: {
+        bail: true,
+      }
+    },
     files: [{
       pattern: 'test/fixtures/**/*',
       watched: false,
