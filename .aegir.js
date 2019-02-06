ef6ec28756cb5746de549d6b199d549c1b213750
Hugo Dias <hugomrdias@gmail.com>
2019-02-06 18:18:36 +0000

    ci: remove custom karma stuff

--- .aegir.js
+++ .aegir.js
@@ -10,41 +10,7 @@ module.exports = {
       watched: false,
       served: true,
       included: false
-    }],
-    reporters: ['coverage-istanbul'],
-    coverageIstanbulReporter: {
-      reports: ['json'],
-      dir: path.join(__dirname, 'coverage'),
-      combineBrowserReports: true,
-      fixWebpackSourcePaths: true,
-    },
-    preprocessors: { 'node_modules/aegir/src/config/karma-entry.js': [ 'webpack', 'sourcemap' ] },
-    webpack: {
-      module: {
-        rules: [
-          // instrument only testing sources with Istanbul
-          {
-            test: /\.js$/,
-            use: { loader: 'istanbul-instrumenter-loader' },
-            include: path.resolve('src/')
-          }
-        ]
-      }
-    },
-    customLaunchers: {
-      ChromeDocker: {
-        base: 'ChromeHeadless',
-        // We must disable the Chrome sandbox when running Chrome inside Docker (Chrome's sandbox needs
-        // more permissions than Docker allows by default)
-        flags: ['--no-sandbox']
-      }
-    },
-    client: {
-      mocha: {
-        bail: true,
-      }
-    },
-    singleRun: true
+    }]
   },
   hooks: {
     browser: {
