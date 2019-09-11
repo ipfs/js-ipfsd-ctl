cb321f42129037c06573adf2f8893b90a90d52bc
Alex Potsides <alex@achingbrain.net>
2019-09-11 11:55:19 +0100

    chore: upgrade http client (#371)
    * chore: upgrade http client
    
    * chore: increase bundlesize

--- .aegir.js
+++ .aegir.js
@@ -4,7 +4,7 @@ const createServer = require('./src').createServer
 
 const server = createServer() // using defaults
 module.exports = {
-  bundlesize: { maxSize: '280kB' },
+  bundlesize: { maxSize: '281kB' },
   karma: {
     files: [{
       pattern: 'test/fixtures/**/*',
