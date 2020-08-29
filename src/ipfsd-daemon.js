79dc6c65c828335fa0fd4b01b4295395821148c6
bluelovers <codelovers@users.sourceforge.net>
2020-08-30 01:54:03 +0800

    feat: add --migrate

--- src/ipfsd-daemon.js
+++ src/ipfsd-daemon.js
@@ -176,6 +176,9 @@ class Daemon {
     if (opts.EXPERIMENTAL && opts.EXPERIMENTAL.ipnsPubsub) {
       args.push('--enable-namesys-pubsub')
     }
+    if (opts.repoAutoMigrate) {
+      args.push('--migrate')
+    }
 
     // Check if a daemon is already running
     const api = checkForRunningApi(this.path)
