d2f623a82f33d63959f95a4238cc8e2d6f0415a0
Alex Potsides <alex@achingbrain.net>
2021-03-26 09:35:04 +0000

    chore: use .create function for http client and ipfs client (#616)
    ipfs/js-ipfs#3550 switches away from default exports for `ipfs-http-client`
    and `ipfs-client` to enable an easier transition to es6 modules.
    
    All `ipfs` modules now export a `.create` factory function which returns
    instances of the client module.
    
    BREAKING CHANGE: expects `ipfs-http-client` and `ipfs-client` to export a `.create` function

--- src/ipfsd-client.js
+++ src/ipfsd-client.js
@@ -76,12 +76,12 @@ class Client {
    */
   _createApi () {
     if (this.opts.ipfsClientModule && this.grpcAddr && this.apiAddr) {
-      this.api = this.opts.ipfsClientModule({
+      this.api = this.opts.ipfsClientModule.create({
         grpc: this.grpcAddr,
         http: this.apiAddr
       })
     } else if (this.apiAddr) {
-      this.api = this.opts.ipfsHttpModule(this.apiAddr)
+      this.api = this.opts.ipfsHttpModule.create(this.apiAddr)
     }
 
     if (this.api) {
