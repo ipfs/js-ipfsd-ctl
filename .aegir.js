16d53bec0c9df63d8eb7ada8aba9e02b66aff46d
achingbrain <alex@achingbrain.net>
2021-04-08 17:42:11 +0100

    chore: fix aegir config for browser tests

--- .aegir.js
+++ .aegir.js
@@ -1,13 +1,24 @@
 'use strict'
 
+const path = require('path')
 const getPort = require('aegir/utils/get-port')
 const createServer = require('./src').createServer
+
+/** @type {import('aegir').Options["build"]["config"]} */
+const esbuild = {
+  inject: [path.join(__dirname, 'scripts/node-globals.js')],
+}
 
 module.exports = {
   bundlesize: {
     maxSize: '35kB'
   },
   test: {
+    browser: {
+      config: {
+        buildConfig: esbuild
+      }
+    },
     before: async () => {
       const server = createServer(undefined, {
           ipfsModule: require('ipfs'),
---
+++ scripts/node-globals.js
@@ -0,0 +1,4 @@
+
+// @ts-nocheck
+export const { Buffer } = require('buffer')
+export const __dirname = globalThis.__dirname
