3a78c13c0f242b3864f1436435745da6e4a1b407
achingbrain <alex@achingbrain.net>
2022-09-30 08:42:02 +0100

    chore: fix tests

--- src/endpoint/server.browser.ts
+++ src/endpoint/server.browser.ts
@@ -1,10 +1,20 @@
 /* eslint-disable no-console */
 
+import type { ServerInit } from './server.js'
+
 /**
  * Creates an instance of Server
  */
 class Server {
-  constructor () {
+  private readonly options: ServerInit
+  public port: number
+  public host: string
+
+  constructor (options: ServerInit = { port: 43134, host: 'localhost' }) {
+    this.options = options
+    this.port = this.options.port ?? 43134
+    this.host = this.options.host ?? 'localhost'
+
     console.warn('Server not implemented in the browser')
   }
 
